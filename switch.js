a95450b119f5cc3e6267d7385e3a4e147261e9c6
Kartikjajoria <100478521+Kartikjajoria@users.noreply.github.com>
2023-09-03 20:01:48 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,17 @@
+let a = prompt("what is your age")
+switch(age){
+    case 12:
+        console.log("your age is 12")
+        break
+     case 13:
+        console.log("your age is 13")
+        break
+    case 14:
+        console.log("your age is 14")
+        break
+    case 15:
+        console.log("your age is 15")
+        break
+    default:
+        console.log("your age is not special")    
+}
---
+++ ternery_operator.js
@@ -0,0 +1,3 @@
+let age =20
+let a=age>18?"you can drive":"you cannot drive"
+console.log(a)
