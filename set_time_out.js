0bdd0836cd0372169bb050437ea5f9581b2d76df
Kartikjajoria <100478521+Kartikjajoria@users.noreply.github.com>
2023-09-03 20:30:08 +0530

    Add files via upload

---
+++ set_time_out.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    console.log("hello") //we can also use alert instead of console.log
+}, 7000)
+
+// setinterval
+// setInterval(function(){
+//     console.log("holaaaaa")
+// }, 3000)
