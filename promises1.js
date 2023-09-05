fed4aef6883c6ba101f34f2a3b703dd765c292ec
Kartikjajoria <100478521+Kartikjajoria@users.noreply.github.com>
2023-09-05 20:32:47 +0530

    Update promises1.js

--- promises1.js
+++ promises1.js
@@ -1,4 +1,14 @@
-let p = new Promise((resolve, reject)=>{
+et p1 = new Promise((resolve, reject)=>{
+    console.log("promise is pending")
+    setTimeout(()=>{
+        console.log("i am a promise and i am fulfilled")
+        resolve(true)
+        // console.log("i a promise and i am rejected")
+        // reject(new Error("i am an error please bear me"))
+    }, 5000)
+})
+
+let p2 = new Promise((resolve, reject)=>{
     console.log("promise is pending")
     setTimeout(()=>{
         // console.log("i am a promise and i am fulfilled")
@@ -7,4 +17,9 @@ let p = new Promise((resolve, reject)=>{
         reject(new Error("i am an error please bear me"))
     }, 5000)
 })
-
+p1.then((value)=>{ 
+console.log(value)
+})
+p2.catch((error)=>{    // will catch the and error will not be shown in console
+    console.log("some error occured in p2")
+})
